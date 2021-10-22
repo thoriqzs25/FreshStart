ed11eefcaac1738bf422aef09cefba6598f02f1c
thoriqzs25 <thoriqzahfran@yahoo.com>
2021-10-22 09:05:07 +0700

    adding multiple state in reducer redux

--- latihan-18/01-starting-project/src/components/Counter.js
+++ latihan-18/01-starting-project/src/components/Counter.js
@@ -4,8 +4,11 @@ import { useSelector, useDispatch } from 'react-redux';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter);
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
@@ -15,12 +18,16 @@ const Counter = () => {
     dispatch({ type: 'decrement' });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', payloads: 10 })
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- latihan-18/01-starting-project/src/store/index.js
+++ latihan-18/01-starting-project/src/store/index.js
@@ -1,12 +1,31 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
     switch(action.type) {
         case 'increment':
-            return {counter: state.counter + 1};
+            return {
+                counter: state.counter + 1,
+                showCounter: state.showCounter
+            };
+
+        case 'increase':
+            return {
+                counter: state.counter + action.payloads,
+                showCounter: state.showCounter
+            };
 
         case 'decrement':
-            return {counter: state.counter - 1}
+            return {
+                counter: state.counter - 1,
+                showCounter: state.showCounter
+            };
+        
+        case 'toggle':
+            return {
+                counter: state.counter,
+                showCounter: !state.showCounter
+            }
+
         default:
             return state
     }
