4d19b1a1305a912ce15ed1a233e9fb84dd94d64f
thoriqzs25 <thoriqzahfran@yahoo.com>
2021-10-23 12:02:03 +0700

    adding toggle action in uiSlice.js

--- latihan-19/01-starting-project/src/App.js
+++ latihan-19/01-starting-project/src/App.js
@@ -2,10 +2,14 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
+import { useSelector } from 'react-redux';
+
 function App() {
+  const uiVisible = useSelector(state => state.ui.isVisible);
+
   return (
     <Layout>
-      <Cart />
+      { uiVisible && <Cart /> }
       <Products />
     </Layout>
   );
--- latihan-19/01-starting-project/src/components/Cart/CartButton.js
+++ latihan-19/01-starting-project/src/components/Cart/CartButton.js
@@ -1,8 +1,18 @@
 import classes from './CartButton.module.css';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const cartHandler = (event) => {
+    event.preventDefault();
+
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={cartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
