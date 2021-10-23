23095d32d792c7db5e7d4b56e9c2dce76ac71558
thoriqzs25 <thoriqzahfran@yahoo.com>
2021-10-23 13:08:09 +0700

    ngikutin instruksi yang 20 menit

--- latihan-19/01-starting-project/src/store/cart-slice.js
+++ latihan-19/01-starting-project/src/store/cart-slice.js
@@ -1,1 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+    name: 'cart',
+    intialState: { 
+        item: [],
+        totalQuantity: 0
+    },
+    reducers: {
+        addItem(state, action) {
+            const newItem = action.payload;
+            const existingItem = state.item.find(item => {item.id === newItem.id});
+            if(!existingItem) {
+                state.item.push({
+                    itemId: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.title,
+                })
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice = existingItem.totalPrice + existingItem.price;
+            }
+        }
+    }
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice.reducer;
